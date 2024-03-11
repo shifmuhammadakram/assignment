e30a9b0183cdfdb841282743f7a3b3ad3a58f083
shifmuhammadakram <161406150+shifmuhammadakram@users.noreply.github.com>
2024-03-11 11:59:40 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var array = ["shifa", "maryam", "alishba", 5, 7, 8, 6];
+console.log(array[2]);
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[3]);
+console.log(array[4]);
+console.log(array[5]);
+console.log(array[6]);
